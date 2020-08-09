ffd0efa3fb3e6595e4c9bcbe0bfc4de4868f3682
tnlp <2803633927@qq.com>
2020-08-09 15:58:39 +0800

    101

---
+++ 101/app.py
@@ -0,0 +1,90 @@
+from flask import Flask,request,redirect,url_for,abort,make_response,json
+from flask import jsonify,session
+import os
+app=Flask(__name__)
+
+@app.route('/hello')
+
+def hello():
+    name=request.args.get('name','flask')
+    return '<h1>hello,%s</h1>' % name
+@app.route('/hellos',methods=['GET','POST'])
+def hellos():
+    #return 'hello world',201 
+    name=request.args.get('name')
+    if name is None:
+        name=request.cookies.get('name','Human')
+    return '<h1>hello, %s</h1>' % name
+@app.route('/redirects')
+def redirects():
+    return redirect('https://www.baidu.com/')
+
+
+@app.route('/test')
+def test():
+    return redirect(url_for('hello'))
+
+
+@app.route('/goback/<int:year>')
+def go_bback(year):
+    return '<p> Welcome to %d </p>' %(2018-year)
+@app.route('/foo')
+
+def foo():
+    response=make_response('hello,word')
+    response.mimetype='test/plain'
+    return response
+
+@app.route('/foo1')
+def foo1():
+    data={
+        'name':'Grey Li',
+        'gender':'male'
+    }
+    response=make_response(json.dumps(data))
+    response.mimetype='application/json'
+    return response
+@app.route('/foo2')
+def foo2():
+    return jsonify({'name':'Eric Zhou','age':22,'gender':'male'})
+
+@app.route('/set/<name>')
+def set_cookie(name):
+    response=make_response(redirect(url_for('hellos')))
+    response.set_cookie('name',name)
+    return response
+
+@app.route('/404')
+def not_found():
+    abort(404)
+
+app.secret_key='devops@2020'
+
+@app.route('/login')
+def login():
+    session['logged_in']=True
+    return redirect(url_for('demo'))
+@app.route('/demo')
+def demo():
+    name=request.args.get('name')
+    if name is None:
+        name=request.cookies.get('name','test')
+        response='hello ,%s' % name
+        if 'logged_in' in session:
+            response+='[Authenticated]'
+        else:
+            response+='[Not Authenticated]'
+        return response
+@app.route('/admin')
+def admin():
+    if 'logged_in' not in session:
+        abort(403)
+    return 'welcome to admin page'
+
+@app.route('/logout')
+def logout():
+    if 'logged_in' in session:
+        session.pop('logged_in')
+    return redirect(url_for('demo'))    
+if __name__ == '__main__':
+    app.run(host="127.0.0.1", port=7000,debug=True)
